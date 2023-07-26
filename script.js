5ff4e349d32524fa2b0db2fec85bd0732657d346
Saharah-Henry <henrysaharah07@gmail.com>
2023-07-25 19:47:36 -0500

    Remove "Current" button

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ displayDate();
 
 function showTemp(response) {
   console.log(response);
+
   let temperature = document.querySelector("#temp");
   temperature.innerHTML = Math.round(response.data.temperature.current);
   let weather = document.querySelector("#weather");
@@ -44,6 +45,7 @@ function showTemp(response) {
     "src",
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
+  celsiusTemp = response.data.temperature.current;
 }
 function searchCity(event) {
   event.preventDefault();
@@ -56,45 +58,6 @@ function searchCity(event) {
   let apiUrl = "https://api.shecodes.io/weather/v1/current?";
   axios.get(`${apiUrl}query=${city}&key=${apiKey}&units=metric`).then(showTemp);
 }
-
-let searchTab = document.querySelector("#search-tab");
-searchTab.addEventListener("submit", searchCity);
-
-function currentCity(response) {
-  let temperature = document.querySelector("#temp");
-  temperature.innerHTML = Math.round(response.data.temperature.current);
-  let weather = document.querySelector("#weather");
-  weather.innerHTML = ` ${response.data.condition.description}`;
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = ` Humidity: ${response.data.temperature.humidity}`;
-  let windspeed = document.querySelector("#wind");
-  windspeed.innerHTML = ` Wind: ${Math.round(response.data.wind.speed)} m/s`;
-  let feel = document.querySelector("#est_temp");
-  feel.innerHTML = ` Feels like: ${Math.round(
-    response.data.temperature.feels_like
-  )}°`;
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = response.data.city;
-  let iconElement = document.querySelector("#weather-icon");
-  iconElement.setAttribute(
-    "src",
-    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
-  );
-  celsiusTemp = response.data.temperature.current;
-}
-function currentCoordinates(position) {
-  let lon = position.coords.longitude;
-  let lat = position.coords.latitude;
-  let apiKey = "664c139fao009ab4f0e6872f57fc202t";
-  let apiUrl = "https://api.shecodes.io/weather/v1/current?";
-  axios
-    .get(`${apiUrl}lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`)
-    .then(currentCity);
-}
-function currentPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(currentCoordinates);
-}
 function degreeF(event) {
   event.preventDefault();
   let temperature = document.querySelector("#temp");
@@ -106,11 +69,10 @@ function degreeC(event) {
   let temperature = document.querySelector("#temp");
   temperature.innerHTML = Math.round(celsiusTemp);
 }
-
 let celsiusTemp = null;
 
-let current = document.querySelector("#current-tab");
-current.addEventListener("submit", currentPosition);
+let searchTab = document.querySelector("#search-tab");
+searchTab.addEventListener("submit", searchCity);
 
 let fahrenheitLink = document.querySelector("#fahrenheit");
 fahrenheitLink.addEventListener("click", degreeF);
